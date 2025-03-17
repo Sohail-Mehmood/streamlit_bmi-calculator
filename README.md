7a093106f63b08d5985e313e9c65978bab4b4dee
Sohail-Mehmood <khan.sb1210@gmail.com>
2025-03-17 10:42:53 +0500

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# streamlit_bmi-calculator
-BMI Calculator (Streamlit Web App) This repository contains a BMI (Body Mass Index) Calculator built using Streamlit. It allows users to input their weight (in kg) and height (in feet &amp; inches), then calculates and categorizes their BMI.
--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def calculate_bmi(weight, feet, inches):
     height_meters = (feet * 0.3048) + (inches * 0.0254)
     bmi = weight / (height_meters ** 2)
     return bmi
-
-# User inputs
 weight = st.number_input("Enter your weight in kg:", min_value=1.0, max_value=500.0, value=75.0, step=0.1, key="weight_input")
 feet = st.number_input("Enter your height (feet):", min_value=2, max_value=8, value=5, key="feet_input")
 inches = st.number_input("Enter additional height (inches):", min_value=0, max_value=11, value=6, key="inches_input")
@@ -21,10 +19,10 @@ st.write(f"**Your BMI is:** {result:.2f}")
 
 # Interpretation of BMI categories
 if result < 18.5:
-    st.write("⚠️ **Hey, Man you are underweight.**")
+    st.write("**Hey, Man you are underweight.**")
 elif 18.5 <= result <= 24.9:
-    st.write("✅ **Hey, Man you have a normal weight.**")
+    st.write("**Hey, Man you have a normal weight.**")
 elif 25 <= result <= 29.9:
-    st.write("⚠️ **Hey, Man you are overweight.**")
+    st.write("**Hey, Man you are overweight.**")
 else:
-    st.write("🚨 **Hey, Man you are obese.**")
+    st.write("**Hey, Man you are obese.**")
